1d4aa6345b54c96115c24aad01c7184fbc035f0d
Santi Aguilera <aguilerasanti@hotmail.com>
2016-04-21 22:25:15 -0300

    EventManager is threadsafe now

--- app/src/main/java/com/santiago/event/EventManager.java
+++ app/src/main/java/com/santiago/event/EventManager.java
@@ -9,9 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * TODO I think it cant have race conditions on the broadcast/add/rm. But pay atention in case it happens
- *
  * Class in charge of receiving events (from listeners), manage them and/or broadcast them to the other listeners.
+ *
+ * @note Thread-safe.
  *
  * Created by santiaguilera@theamalgama.com on 01/03/16.
  */
@@ -21,7 +21,7 @@ public class EventManager implements EventListener {
 
     private EventDispatcher dispatcher; //Dude in charge of dispatching events
 
-    private List<Object> objectsListening = new ArrayList<>(); //List of all the objects willing to receive events
+    private final List<Object> objectsListening = new ArrayList<>(); //List of all the objects willing to receive events
 
     public EventManager(@NonNull Context context){
         this.context = context;
@@ -39,7 +39,9 @@ public class EventManager implements EventListener {
      * @param object
      */
     public void addListener(@NonNull Object object){
-        objectsListening.add(object);
+        synchronized (objectsListening) {
+            objectsListening.add(object);
+        }
     }
 
     /**
@@ -48,12 +50,18 @@ public class EventManager implements EventListener {
      * @return if it was successfully removed
      */
     public boolean removeListener(@NonNull Object object) {
-        return objectsListening.remove(object);
+        synchronized (objectsListening) {
+            return objectsListening.remove(object);
+        }
     }
 
     /**
      * Method called (by one sending an event) for the purpose of managing that event, and/or else broadcast it to the
      * list of classes listening
+     *
+     * @note Since we could be adding or rm listeners from other threads, we create a copy of the current listeners
+     * and iterate over them.
+     *
      * @param event
      */
     @Override
@@ -61,8 +69,13 @@ public class EventManager implements EventListener {
         //Dispatch the event to ourselves
         dispatcher.dispatchEvent(event, this);
 
+        List<Object> listenersCopy;
+        synchronized (objectsListening) {
+            listenersCopy = new ArrayList<>(objectsListening);
+        }
+
         //Iterate through all the objects listening and dispatch it too
-        for(Object object : new ArrayList<>(objectsListening))
+        for(Object object : listenersCopy)
             if(object != null)
                 dispatcher.dispatchEvent(event, object);
     }
